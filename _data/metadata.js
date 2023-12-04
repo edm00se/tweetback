badae8a9601fc988af27372de075e94616e81983
Eric McCormick <edm00se@users.noreply.github.com>
2023-12-04 19:34:28 +0000

    reset customizations

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "edm00se", // No leading @ here
+	homeLabel: "edm00se.codes",
+	homeUrl: "https://edm00se.codes/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
